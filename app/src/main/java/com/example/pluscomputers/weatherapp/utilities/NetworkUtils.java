72dfbc66a994f53bde177f0981bfb3b55ddf2b0a
Arianit Lubishtani <arianit.lu@hotmail.com>
2018-11-05 21:26:30 +0100

    Build proper weather URL

--- app/src/main/java/com/example/pluscomputers/weatherapp/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/pluscomputers/weatherapp/utilities/NetworkUtils.java
@@ -14,9 +14,8 @@ public final class  NetworkUtils {
 
     private static final String TAG = NetworkUtils.class.getSimpleName();
 
-    // http://api.worldweatheronline.com/premium/v1/weather.ashx?key=ec6a2189bd7e486588b133648182410
-    // &q=Prizren&format=json&num_of_days=5&showlocaltime=yes
-
+//     http://api.worldweatheronline.com/premium/v1/weather.ashx?key=ec6a2189bd7e486588b133648182410
+//     &q=Prizren&format=json&num_of_days=5&showlocaltime=yes
 
     // Keys for URI
     private static final String WEATHER_BASE_URL = "http://api.worldweatheronline.com/premium/v1/weather.ashx";
@@ -24,7 +23,7 @@ public final class  NetworkUtils {
     private static final String API_KEY_PARAM = "key";
     private static final String apiKey = "ec6a2189bd7e486588b133648182410";
 
-    private static final String CITY_KEY_PARAM = "q";
+    private static final String QUERY = "q";
     private static final String cityKey = "Prizren";
 
     private static final String FORMAT_KEY = "format";
@@ -36,10 +35,10 @@ public final class  NetworkUtils {
     private static final String LOCAL_TIME = "showlocaltime";
     private static final String localTimeKey = "yes";
 
-    public static URL buildWeatherUrl(String city) {
+    public static URL buildWeatherCityUrl(String city) {
         Uri builtUri = Uri.parse(WEATHER_BASE_URL).buildUpon()
                 .appendQueryParameter(API_KEY_PARAM, apiKey)
-                .appendQueryParameter(CITY_KEY_PARAM,city)
+                .appendQueryParameter(QUERY,city)
                 .appendQueryParameter(FORMAT_KEY,formatKey)
                 .appendQueryParameter(NUM_DAYS,numDaysKey)
                 .appendQueryParameter(LOCAL_TIME,localTimeKey)
@@ -51,7 +50,25 @@ public final class  NetworkUtils {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        Log.v(TAG, "Build movies URI: " + url);
+        Log.v(TAG, "Build weather city URI: " + url);
+        return url;
+    }
+    public static URL buildWeatherGpsUrl(long latitude,long longitude) {
+        Uri builtUri = Uri.parse(WEATHER_BASE_URL).buildUpon()
+                .appendQueryParameter(API_KEY_PARAM, apiKey)
+                .appendQueryParameter(QUERY,latitude+ "," + longitude)
+                .appendQueryParameter(FORMAT_KEY,formatKey)
+                .appendQueryParameter(NUM_DAYS,numDaysKey)
+                .appendQueryParameter(LOCAL_TIME,localTimeKey)
+                .build();
+        Log.i(TAG,builtUri.toString());
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        Log.v(TAG, "Build weather gps URI: " + url);
         return url;
     }
 
